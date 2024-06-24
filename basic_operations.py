1496d436142c37c6a090b1b331e145ae5d1f6f3f
ArabiAyoub <ayoub.arabi.98@gmail.com>
2024-06-24 21:06:43 +0100

    first commit

---
+++ basic_operations.py
@@ -0,0 +1,15 @@
+# Define the variables
+number1 = 10
+number2 = 5
+
+# Calculate the addition
+sum_result = number1 + number2
+print(f"Addition of {number1} and {number2} is {sum_result}")
+
+# Calculate the subtraction
+difference_result = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+
+# Calculate the multiplication
+product_result = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {product_result}")
